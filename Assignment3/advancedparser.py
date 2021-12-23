feb0047548369ee1624114ce64c33fe847531342
siphosethu1998 <siphosethu1998@gmail.com>
2021-12-23 13:42:51 +0200

    added advancedparser.py

---
+++ Assignment3/advancedparser.py
@@ -0,0 +1,13 @@
+'''
+ This program asks the user to input their ID number and does two things
+
+  (i) using the check digit, it determines whether the number is valid; 
+
+  (ii) if the number is valid then it decodes and prints out the user’s date of birth, gender, 
+citizenship status.
+'''
+
+# Siphosethu shumani
+# 2021/12/23
+
+

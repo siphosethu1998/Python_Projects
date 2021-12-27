5045345769a761089c2584856e1f4f8ab9b110d0
siphosethu1998 <siphosethu1998@gmail.com>
2021-12-27 14:30:03 +0200

    completed and tested inchesmetres.py

---
+++ Assignment3/inchesmetres.py
@@ -0,0 +1,28 @@
+# This program displays a conversion table for inches to metres
+# Siphosethu shumani
+# 2021/12/27
+
+""" input """
+
+min_inch = int(input("Enter the minimum number of inches (not less than 0): \n"))
+
+max_inch = int(input("Enter the maximum  number of inches (not greater than 11): \n"))
+    
+""" Process and output """
+inches_to_metres = 0
+
+# for inches
+print("Inches: ", end="")
+
+for inch in range(min_inch, max_inch+1):
+    
+    print(f"{inch:4}", end=" ")
+
+print()
+
+# for metres
+print("Metres: ", end="")
+
+for inch in range(min_inch, max_inch+1):
+    inches_to_metres = inch*0.0254
+    print(f"{inches_to_metres:4.2f}", end=" ")

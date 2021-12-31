013e97f5446cf65e34dd7424760d675026409f9d
siphosethu1998 <siphosethu1998@gmail.com>
2021-12-31 09:41:03 +0200

    added frogsandtoads.py

---
+++ assignment4/frogsandtoads.py
@@ -0,0 +1,14 @@
+"""
+    - This program allows a person to play a game of Frogs and Toads using the computer. 
+    - The program should ask for the number of frogs and the number of toads, and
+    then repeatedly print the puzzle state and ask the user for the index of a frog or toad to move.
+
+    Author : Siphosethu Shumani
+
+    Date : 2021/12/31
+"""
+
+""" Input """
+
+
+""" Process and Output """

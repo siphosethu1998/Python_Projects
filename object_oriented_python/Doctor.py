45ddd69adef4d6189e1c43796a2767dc843e16cd
siphosethu1998 <siphosethu1998@gmail.com>
2022-01-28 16:40:04 +0200

    completed and tested Doctor.py

---
+++ object_oriented_python/Doctor.py
@@ -0,0 +1,30 @@
+"""
+    Author : Siphosethu Shumani
+    Date : 28 Jan 2022
+"""
+
+from Person import Person
+
+class Doctor(Person):
+    def __init__(self, doctor_id, address, n='unknown',a=0):
+        super().__init__(n, a)
+        self.doctor_id = doctor_id
+        self.address = address
+
+    def display(self):
+        print('Name:',self.name)
+        print('Age:',self.age)
+        print("Doctor's id:",self.doctor_id)
+        print("address:",self.address)
+
+    def __str__(self):
+        return self.name + ', ' + str(self.age) + ", " + str(self.doctor_id) + ", " + self.address
+
+
+# main method is for testing the Doctor Class
+def main():
+    d = Doctor(98765, "30 avenue", "John", 35)
+    print(d)
+    d.display()
+
+main()
